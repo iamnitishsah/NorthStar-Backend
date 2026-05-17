78616bd8369e590eacc0fb473da62e9656ac1aa6
Nitish Kumar <iamnitishsah12@gmail.com>
2026-05-17 08:42:18 +0530

    minor fixes

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
+@app.get("/", tags=["Health Check"])
 async def root():
     return {"message": "NorthStar API Gateway is live"}
 
